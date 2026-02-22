1bcac997e993d1cd84386525ed55deaead886428
2SLI <79351666+2SLI@users.noreply.github.com>
2026-02-23 07:29:52 +0900

    aqw

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDx77BlAAUNfVDD53XA7Ro_-L_-OPvix68",
+  authDomain: "mewanwellpower.firebaseapp.com",
+  projectId: "mewanwellpower",
+  storageBucket: "mewanwellpower.firebasestorage.app",
+  messagingSenderId: "281078326402",
+  appId: "1:281078326402:web:d82f67eb30a2dae46a409b"
+};
